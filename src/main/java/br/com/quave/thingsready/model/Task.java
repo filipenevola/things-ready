6f7d5a047b557e76dd7f188427c7a43a1c5be659
Filipe Névola <filipe.nevola@tecsinapse.com.br>
2013-06-14 15:50:05 -0300

    setup javaee 7

--- src/main/java/br/com/quave/thingsready/model/Task.java
+++ src/main/java/br/com/quave/thingsready/model/Task.java
@@ -4,15 +4,28 @@
  */
 package br.com.quave.thingsready.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author filipe
  */
-public class Task {
+@Entity
+public class Task implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
+
+    public Task() {
+    }
 
     public Task(Long id, String title, String description) {
         this.id = id;
@@ -31,4 +44,28 @@ public class Task {
     public String getDescription() {
         return description;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Task other = (Task) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
--- src/main/java/br/com/quave/thingsready/service/TaskService.java
+++ src/main/java/br/com/quave/thingsready/service/TaskService.java
@@ -5,9 +5,14 @@
 package br.com.quave.thingsready.service;
 
 import br.com.quave.thingsready.model.Task;
-import java.util.Arrays;
 import java.util.List;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -16,8 +21,23 @@ import javax.inject.Named;
 @Named
 public class TaskService {
 
+    @PersistenceContext
+    private EntityManager em;
+
     public List<Task> all() {
-        return Arrays.asList(new Task(1L, "Fazer cadastro de cliente", "Cadastrar os clientes no sistema"));
+//        return Arrays.asList(new Task(1L, "Fazer cadastro de cliente", "Cadastrar os clientes no sistema"));
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Task> query = builder.createQuery(Task.class);
+        Root<Task> from = query.from(Task.class);
+        CriteriaQuery<Task> select = query.select(from);
+
+        TypedQuery<Task> typedQuery = em.createQuery(select);
+        List<Task> results = typedQuery.getResultList();;
+        return results;
+    }
+    
+    public void persist(Task task) {
+        em.persist(task);
     }
 
     public Task getById(Long taskId) {
